a199a5bb2b238a23b9cc59a062464dc6eacf8356
Daan van de Haar <43374504+DaanvandeHaar@users.noreply.github.com>
2021-01-03 19:53:19 +0100

    Update repository.go

--- pkg/storage/repository.go
+++ pkg/storage/repository.go
@@ -10,18 +10,15 @@ type Storage struct {
 }
 
 func NewStorage() *Storage {
-
+	
 	const (
 		dbUser					= "postgres"
 		dbPwd 					= "admin"
-		instanceConnectionName			= "bep-lingo:europe-west1:lingodb"
+		instanceConnectionName	= "bep-lingo:europe-west1:lingodb"
 		dbName					= "lingo_db"
 	)
 
-	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
-	if !isSet {
-		socketDir = "/cloudsql"
-	}
+	socketDir := "/cloudsql"
 
 	var dbURI string
 	dbURI = fmt.Sprintf("user=%s password=%s database=%s host=%s/%s", dbUser, dbPwd, dbName, socketDir, instanceConnectionName)
